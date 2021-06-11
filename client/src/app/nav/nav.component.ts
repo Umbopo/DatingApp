07edbc5c0eada1029ee98399491c7c9fd04f1bce
Nico Hougaard <nico@ipingroup.com>
2021-06-11 16:43:20 +0200

    End of section 9

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -20,7 +20,7 @@ export class NavComponent implements OnInit {
 
   login() {
     this.accountService.login(this.model).subscribe(response => {
-      this.router.navigateByUrl('/memebrs');
+      this.router.navigateByUrl('/members');
     })
   }
 
